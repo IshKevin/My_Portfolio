f64738df3c9639a40c5754215409166b688c040c
IshKevin <carterk279@gmail.com>
2022-12-15 19:19:49 +0200

    fixing bugs

--- contact.js
+++ contact.js
@@ -1,19 +1,16 @@
-function myFunction() {
+function namevalid() {
     let x = document.getElementById("in1").value;
     let text;
     if (x=="") {
       text = "Enter your name Please";
     } 
     document.getElementById("on").innerHTML = text;
-    
     let y = document.getElementById("in2").value;
     let text2;
-    if (!(y.includes('@'))) {
-      text2 = "Enter a valid email";
-    } else(y=="")
-    {
+    if (y == " ") {
+        
         text2 = "Enter your email Please";  
     }
 
     document.getElementById("in2").innerHTML = text2;
-  }
+}  
